68b721d1e97768b5eeecd7550ade1e9e5ecce952
bapuniswain20 <bapuniswain83@gmail.com>
2025-12-19 17:21:14 +0530

    create server

---
+++ db.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: process.env.DB_HOST ,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect()
+.then(() => {
+  console.log("Connected to the database successfully.");
+})
+.catch((err) => {
+  console.error("Database connection error:", err.stack);
+});
+
+module.exports = pool;
---
+++ index.js
@@ -0,0 +1,22 @@
+ require("dotenv").config();
+ const pool=require('./db');
+
+ const express=require('express');
+    const app=express();
+    app.use(express.json());  
+    
+    
+   app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    console.log(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Database error" });
+  }
+});
+port=3000
+
+    app.listen(port,()=>{
+        console.log(`server is running on port ${port}`);
+    });
