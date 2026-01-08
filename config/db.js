c3330c30550728e5cfc604970f94ff8e4dfb96ab
parhirashmita <rashmitaparhi60@gmail.com>
2026-01-08 20:38:14 +0530

    crud
    crud

--- config/db.js
+++ config/db.js
@@ -1,21 +1,16 @@
-const { client } = require("pg");
-const client = new Client ({
+const { Pool } = require("pg");
+const pool = new Pool ({
   host: process.env.DB_HOST,
   user: "postgres",
   port: 5432,
-  password: "puja@123",
+  password: "puja123",
   database: "Social_media",
 });
-async function connectDB() {
+(async () => {
   try {
-    await client.connect();
-    console.log("Connected to postgreSQL");
-
-    const res = await client.query("SELECT NOW()");
+ const res = await pool.query("SELECT NOW()");
     console.log(res.rows);
   } catch (error) {
     console.error("DB connection error:", error.message);
   }
-}
-  connectDB();
-  module.exports = client;
+})();
