aad2ec93d6aa52a91356f73f88569d7f539a93d9
subratbaiyari <22mca084.subratbaiyari@giet.edu>
2026-01-10 04:58:56 -0500

    Update user routes.js

--- user routes.js
+++ user routes.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const UserController = require('../controller/user.controller');
 
-const UserController = require('../controller/userController');
-
-/* =========================
-   USER ROUTES
-========================= */
-
-// Get users
-router.get('/users', UserController.getUser);
-
-// Create user
 router.post('/users', UserController.createUser);
+router.get('/users', UserController.getAllUsers);
+router.get('/users/:id', UserController.getUserById);
+router.put('/users/:id', UserController.updateUser);
+router.delete('/users/:id', UserController.deleteUser);
 
 module.exports = router;
