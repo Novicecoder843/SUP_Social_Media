a5c5c4a5f3c11329af3e088c881c655ba61a2740
Ansuman63 <ansumanbis90@gmail.com>
2026-02-18 16:44:22 +0530

    DBconnection

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+const pool = require("./config/db");
+
+pool.query("SELECT NOW()")
+  .then(res => {
+    console.log("✅ DB Connected at:", res.rows[0]);
+  })
+  .catch(err => {
+    console.log("❌ DB Not Connected:", err.message);
+  });
