8a672e6669e79b4e1fbabad8dad23ae4907f7117
Ansuman63 <ansumanbis90@gmail.com>
2026-02-18 12:06:06 +0530

    deketed

--- routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.router()
-
-const Usercontroller = require('../controller/userController')
-
-router.get('/getuser',Usercontroller.getuser)
-
-router.get('/createuser',Usercontroller.createuser)
