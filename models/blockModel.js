1c95ca2ce9c9609ecfb1b305b06096ae0c969407
subhashreeparhi <subhashreeparhi226@gmail.com>
2026-03-27 16:01:42 +0530

    profile

--- models/blockModel.js
+++ models/blockModel.js
@@ -1,38 +1,39 @@
 const db = require("../config/db");
 
-exports.toggleBlock = async (userId, blockedUserId) => {
-  // check if already blocked
+exports.toggleBlock = async (blocker_id, blocked_id) => {
+
   const check = await db.query(
-    `SELECT id FROM user_blocks WHERE user_id=$1 AND blocked_user_id=$2`,
-    [userId, blockedUserId]
+    `SELECT * FROM public.user_blocks WHERE blocker_id=$1 AND blocked_id=$2`,
+    [blocker_id, blocked_id]
   );
 
   if (check.rows.length) {
     // unblock
     await db.query(
-      `DELETE FROM user_blocks WHERE user_id=$1 AND blocked_user_id=$2`,
-      [userId, blockedUserId]
+      `DELETE FROM public.user_blocks WHERE blocker_id=$1 AND blocked_id=$2`,
+      [blocker_id, blocked_id]
     );
-    return "unblocked";
+
+    return { message: "User unblocked successfully" };
   }
 
   // block user
   await db.query(
-    `INSERT INTO user_blocks (user_id, blocked_user_id) VALUES ($1, $2)`,
-    [userId, blockedUserId]
+    `INSERT INTO public.user_blocks (blocker_id, blocked_id) VALUES ($1, $2)`,
+    [blocker_id, blocked_id]
   );
 
-  // remove follow relations (both sides)
+  // remove follow relations
   await db.query(
     `
     DELETE FROM user_followers 
     WHERE 
-      (user_id=$1 AND follower_id=$2)
+      (following_id=$1 AND follower_id=$2)
       OR
-      (user_id=$2 AND follower_id=$1)
+      (following_id=$2 AND follower_id=$1)
     `,
-    [userId, blockedUserId]
+    [blocker_id, blocked_id]
   );
 
-  return "blocked";
+  return { message: "User blocked successfully" };
 };
