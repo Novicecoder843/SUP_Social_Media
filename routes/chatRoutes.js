a4de94ffe9ef1cc391fb06a9bd1e666ba9f24868
bhabasmin <bhabasminbehera@gmail.com>
2026-04-23 15:40:03 +0530

    .

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const chatController = require("../controllers/chatController");
 const auth = require("../middleware/authMiddleware");
-
+const upload = require("../middleware/postUploadMiddleware");
 
 
 router.post("/send", auth, chatController.sendMessage);
@@ -11,3 +11,14 @@ router.get("/group/:groupId",chatController.getGroupMessages);
 router.get("/conversation/:receiver_id", auth, chatController.getConversation);
 router.delete("/message/:id" , auth , chatController.deleteMessage);
 module.exports = router;
+
+
+
+
+// router.post("/upload/" , auth , upload.single("media"),(req,res)=>{
+//     res.jsoin({
+//         success : true,
+//         file : req.file.filename,
+//         path : `/uploads/${req.file.filename}`
+//     });
+// });
